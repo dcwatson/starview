7d21bc5318e452bfb04240534fd2b17d13c97f1e
Dan Watson <dcwatson@gmail.com>
2021-08-11 21:03:31 -0400

    Mount point, docker build, mobile-first

--- starview.py
+++ starview.py
@@ -2,6 +2,7 @@ import os
 
 from starlette.applications import Starlette
 from starlette.config import Config
+from starlette.exceptions import HTTPException
 from starlette.responses import FileResponse, PlainTextResponse
 from starlette.routing import Route
 from starlette.templating import Jinja2Templates
@@ -14,6 +15,10 @@ DOTFILES = config("DOTFILES", cast=bool, default=False)
 IGNORECASE = config("IGNORECASE", cast=bool, default=True)
 DIRSFIRST = config("DIRSFIRST", cast=bool, default=True)
 TITLE = config("TITLE", default="Files")
+MOUNT = config("MOUNT", default="/")
+
+if not MOUNT.endswith("/"):
+    MOUNT += "/"
 
 templates = Jinja2Templates(directory="templates")
 
@@ -21,7 +26,7 @@ templates = Jinja2Templates(directory="templates")
 def filesize(num, suffix="B"):
     for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
+            return "%3.1f %s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, "Y", suffix)
 
@@ -43,7 +48,7 @@ def get_entries(root, parts, selected=None):
             continue
         path = os.path.join(root_path, name)
         is_dir = os.path.isdir(path)
-        href = "/" + "/".join(parts + [name])
+        href = MOUNT + "/".join(parts + [name])
         if is_dir:
             href += "/"
         entries.append(
@@ -52,7 +57,8 @@ def get_entries(root, parts, selected=None):
                 "href": href,
                 "is_dir": is_dir,
                 "is_link": os.path.islink(path),
-                "size": os.path.getsize(path),
+                "size": None if is_dir else filesize(os.path.getsize(path)),
+                "selected": name == selected,
             }
         )
     entries.sort(key=sort_key)
@@ -81,18 +87,20 @@ async def serve(request):
     path = os.path.normpath(os.path.join(root, rel_path))
     if not path.startswith(root):
         return PlainTextResponse("Invalid directory.", status_code=400)
+    if not os.path.exists(path):
+        raise HTTPException(404)
     if os.path.isdir(path):
         hierarchy = []
         selected = None
         # TODO: run this in a ThreadPoolExecutor.
         while path_parts:
             entries = get_entries(root, path_parts, selected=selected)
-            href = "/" + "/".join(path_parts) + "/"
+            href = MOUNT + "/".join(path_parts) + "/"
             selected = path_parts.pop()
             hierarchy.insert(0, (selected, href, entries))
         # Always add the root directory at the start.
         entries = get_entries(root, [], selected=selected)
-        hierarchy.insert(0, (TITLE, "/", entries))
+        hierarchy.insert(0, (TITLE, MOUNT, entries))
         return templates.TemplateResponse(
             "index.html",
             {
@@ -107,8 +115,8 @@ async def serve(request):
 
 
 app = Starlette(
-    debug=True,
+    debug=DEBUG,
     routes=[
-        Route("/{path:path}", serve),
+        Route(MOUNT + "{path:path}", serve),
     ],
 )
